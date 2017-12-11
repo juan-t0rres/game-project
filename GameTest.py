1385c297f16475afd85108e93c40cac4a5f4b58e
juan-t0rres <34136394+juan-t0rres@users.noreply.github.com>
2017-12-11 16:10:43 -0500

    Update GameTest.py

--- GameTest.py
+++ GameTest.py
@@ -16,10 +16,6 @@ class Hero(pygame.sprite.Sprite):
     def update(self,location):
         self.rect.left, self.rect.top = location
 class Projectile:
-    xMove = 0
-    yMove = 0
-    x = 10
-    y = 10
     def __init__(self,y,e, et):#boneless
         if(et==0):
             self.xMove = 20
@@ -43,7 +39,8 @@ class Projectile:
         blue = (0, 255, 0)
         pygame.draw.circle(gd, blue, [self.x,self.y], 5, 5)
 class projectTileList:
-    list = []
+    def __init__(self):
+        self.list = []
     def add(self, other):
         self.list.append(other)
     def tick(self):
@@ -95,7 +92,7 @@ colours=[(0,0,0),(255,0,0),(0,255,0),(0,0,255),(255,255,255)]
 block=10
 displayx=800
 displayy=600
-fps=60
+fps=120
 
 gameDisplay=pygame.display.set_mode((displayx, displayy))
 pygame.display.set_caption("Test")
@@ -114,6 +111,9 @@ time=0
 proj = projectTileList()
 lastPressed = 0
 
+myfont = pygame.font.SysFont("monospace", 42)
+
+score = 0
 while not gameOver:
     proj.tick()
     for event in pygame.event.get():
@@ -146,11 +146,18 @@ while not gameOver:
     time+=1
     y+=dy
     x+=dx
-    if time==100:
+    if time==2:
         zombies.append(Zombies(random.randint(1,6),"Zombie.png",[random.randint(0,800),0],0,0))
         time=1
     for z in zombies:
         z.tick(x,y)
+        for i in range (0,len(proj.list)):
+            if (proj.list[i].x-z.x <= 25 and  proj.list[i].x-z.x >= 0) and (proj.list[i].y - z.y <= 25 and proj.list[i].y-z.y >= 0):
+                if z in zombies:
+                    zombies.remove(z)
+                    score = score + 1
+                print("DELETED")
+    scoreLabel = myfont.render(str(score), 1, (0,0,0))
     if(x<0):
         x=displayx-block
         if(mapX>0):
@@ -177,6 +184,7 @@ while not gameOver:
             y=displayy-block
     gameDisplay.fill(colours[0])
     gameDisplay.blit(Bmap[mapX][mapY].image, Bmap[mapX][mapY].rect)
+    gameDisplay.blit(scoreLabel, (100, 100))
     pygame.draw.rect(gameDisplay, colours[0], [x, y, block, block])
     proj.render(gameDisplay)
     for z in zombies:
